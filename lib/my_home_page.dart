c27307e4ca3d6755886bac912f712e1c425a2d54
Jasper <zhengjasper97@gmail.com>
2023-12-01 17:08:45 +0800

    [MOD] 關閉Debug模式／新增HomePage左側邊欄

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -20,6 +20,33 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+          child: Column(
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(
+                color: Colors.yellow,
+                image: DecorationImage(
+                    image: NetworkImage('url'), fit: BoxFit.cover)),
+            child: ListView(
+              children: const [Text('頭部')],
+            ),
+          ),
+          const ListTile(
+            title: Text('個人中心'),
+            leading: CircleAvatar(
+              child: Icon(Icons.people),
+            ),
+          ),
+          const Divider(),
+          const ListTile(
+            title: Text('系統設置'),
+            leading: CircleAvatar(
+              child: Icon(Icons.settings),
+            ),
+          )
+        ],
+      )),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
