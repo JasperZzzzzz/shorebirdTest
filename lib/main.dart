18af685df06cee58c7c0649d2aef73895d7bdaaa
Jasper <zhengjasper97@gmail.com>
2023-12-05 09:32:30 +0800

    [MOD] add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '作品'),
     );
   }
 }
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -18,58 +18,130 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-          child: Column(
-        children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(
-                color: Colors.yellow,
-                image: DecorationImage(
-                    image: NetworkImage('url'), fit: BoxFit.cover)),
-            child: ListView(
-              children: const [Text('側邊欄')],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        drawer: Drawer(
+            child: Column(
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                  color: Colors.orange,
+                  image: DecorationImage(
+                      image: NetworkImage('url'), fit: BoxFit.cover)),
+              child: ListView(
+                children: const [Text('左邊側邊欄')],
+              ),
             ),
-          ),
-          const ListTile(
-            title: Text('個人中心'),
-            leading: CircleAvatar(
-              child: Icon(Icons.people),
+            const ListTile(
+              title: Text('個人中心'),
+              leading: CircleAvatar(
+                child: Icon(Icons.people),
+              ),
             ),
-          ),
-          const Divider(),
-          const ListTile(
-            title: Text('系統設置'),
-            leading: CircleAvatar(
-              child: Icon(Icons.settings),
-            ),
-          )
-        ],
-      )),
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Divider(),
+            const ListTile(
+              title: Text('系統設置'),
+              leading: CircleAvatar(
+                child: Icon(Icons.settings),
+              ),
+            )
           ],
+        )),
+        onDrawerChanged: (isOpened) {
+          print('onDrawerChanged');
+        },
+        endDrawer: Drawer(
+          child: Column(children: [
+            DrawerHeader(
+                decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                        image: NetworkImage('url'), fit: BoxFit.cover)),
+                child: ListView(
+                  children: const [Text('右邊側邊欄')],
+                )),
+            const ListTile(
+              title: Text('卡片設置'),
+              leading: CircleAvatar(
+                child: Icon(Icons.people),
+              ),
+            ),
+            const Divider(),
+            const ListTile(
+              title: Text('偏好設定'),
+              leading: CircleAvatar(
+                child: Icon(Icons.settings),
+              ),
+            )
+          ]),
+        ),
+        onEndDrawerChanged: (isOpened) {
+          print('onEndDrawerChanged');
+        },
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Center(
+          child: TabBarView(
+            children: [
+              Book(
+                imgSrc: 'https://openhome.cc/Gossip/images/ACL059300.jpg',
+                name: 'Java SE 14 技術手冊',
+              ),
+              Book(
+                imgSrc: 'https://openhome.cc/Gossip/images/ACL054400.jpg',
+                name: 'Python 3.7 技術手冊',
+              ),
+              Book(
+                imgSrc: 'https://openhome.cc/Gossip/images/AEL022800.jpg',
+                name: 'JavaScript 技術手冊',
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+          color: const Color(0XFFFFB2BF),
+          child: const TabBar(tabs: [
+            Tab(
+              text: 'Java',
+            ),
+            Tab(
+              text: 'Python',
+            ),
+            Tab(
+              text: 'JavaScript',
+            )
+          ]),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+class Book extends StatelessWidget {
+  final String imgSrc;
+  final String name;
+
+  const Book({super.key, required this.imgSrc, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Image.network(imgSrc),
+        ),
+        Text(name)
+      ],
     );
   }
 }
